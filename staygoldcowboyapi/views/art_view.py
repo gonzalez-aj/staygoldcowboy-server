2c71b81c3395cb09cb4a9376286a0cfc011e2697
Angie Gonzalez <angiem.go@gmail.com>
2023-06-13 10:51:16 -0500

    ArtView and URLS for GET retrieve single and list All Art works on postman

---
+++ staygoldcowboyapi/views/art_view.py
@@ -0,0 +1,45 @@
+"""View module for handling requests about game types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from staygoldcowboyapi.models import Art
+
+
+class ArtView(ViewSet):
+    """SGC Art view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single art
+
+        Returns:
+            Response -- JSON serialized art
+        """
+        art = Art.objects.get(pk=pk)
+        serializer = ArtSerializer(art)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def list(self, request):
+        """Handle GET requests to get All Art
+
+        Returns:
+            Response -- JSON serialized list of art
+        """
+        arts = Art.objects.all()
+        serializer = ArtSerializer(arts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ArtSerializer(serializers.ModelSerializer):
+    """JSON serializer for Art
+    """
+    class Meta:
+        model = Art
+        fields = (
+            'id', 
+            'fan', 
+            'title', 
+            'creation_date', 
+            'image_url', 
+            'tag'
+            )
